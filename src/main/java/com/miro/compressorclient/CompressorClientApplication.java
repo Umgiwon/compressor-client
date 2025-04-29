340e2d688d1c3421295580b3aa76cc0274a87e2d
umgiwon <ugwzzang@gmail.com>
2025-04-29 17:28:09 +0900

    init

---
+++ src/main/java/com/miro/compressorclient/CompressorClientApplication.java
@@ -0,0 +1,13 @@
+package com.miro.compressorclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompressorClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompressorClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/miro/compressorclient/CompressorClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miro.compressorclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompressorClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
